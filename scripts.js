9a8426e2c67da1622c89c708d5f21949d0d8764d
bertcherry <119577715+bertcherry@users.noreply.github.com>
2023-03-17 00:12:38 -0700

    add submit new books to library

--- scripts.js
+++ scripts.js
@@ -5,14 +5,25 @@ function Book(title, author, pages, read) {
     this.author = author
     this.pages = pages
     this.read = read
-    this.sayInfo = function() {
+    this.printInfo = function() {
         return title + ' by ' + author + ', ' + pages + ' pages, ' + read + '.'
     }
 }
 
 function addBookToLibrary() {
-    //Push new book to library using form information as book arguments and submit button
+    //Push new book to library using form information as book arguments
+    let addedBook = new Book(
+        document.querySelector('#title'),
+        document.querySelector('#author'),
+        document.querySelector('#pages'),
+        document.querySelector('#read')
+    )
+    myLibrary.push(addedBook);
 }
+
+//Listener on submit button pushes new book to library array
+const submitButton = document.querySelector('.submit-btn')
+submitButton.addEventListener('click', addBookToLibrary)
 
 //Example books to populate and test
 const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkein', '295', 'not read yet')
