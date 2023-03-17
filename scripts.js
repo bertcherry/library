83674d817d2ea187890596abc527635d4c0263c4
bertcherry <119577715+bertcherry@users.noreply.github.com>
2023-03-17 16:59:48 -0700

    add new book form and submit action

--- scripts.js
+++ scripts.js
@@ -10,28 +10,32 @@ function Book(title, author, pages, read) {
     }
 }
 
-function addBookToLibrary() {
+function addBookToLibrary(e) {
+    e.preventDefault();
     //Push new book to library using form information as book arguments
     let addedBook = new Book(
-        document.querySelector('#title'),
-        document.querySelector('#author'),
-        document.querySelector('#pages'),
-        document.querySelector('#read')
+        document.querySelector('#title').value,
+        document.querySelector('#author').value,
+        document.querySelector('#pages').value,
+        document.querySelector('#read').value
     )
     myLibrary.push(addedBook);
-    document.getElementById('newBookForm').style.display = 'none';
+    document.getElementById('new-book-form').style.display = 'none';
     //printInfo for addedBook if needed to update display
+    for (const book of myLibrary) {
+        console.log(book.printInfo());
+    }
 }
 
 //Call up a form from a new book button
 function showBookForm() {
     //display:none to display:block/flex/etc toggle on the form
-    document.getElementById('newBookForm').style.display = 'block';
+    document.getElementById('new-book-form').style.display = 'block';
     //include any background color changes desired here
 }
 
-const newButton = document.querySelector('.new-btn')
-newButton.addEventListener('click', showBookForm)
+//const newButton = document.querySelector('.new-btn')
+//newButton.addEventListener('click', showBookForm)
 
 //Listener on submit button pushes new book to library array
 const submitButton = document.querySelector('.submit-btn')
@@ -41,6 +45,9 @@ submitButton.addEventListener('click', addBookToLibrary)
 const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkein', '295', 'not read yet')
 
 //For each book display a line on grid
+for (const book of myLibrary) {
+    console.log(book.printInfo());
+}
 
 //Delete button for each book (data-attribute is index of array?)
 
